1c803088d4d738d08b11b7685234a4edd84daa6d
Akshay Kumar <akshaysharmav7@gmail.com>
2026-09-24 17:07:44 +0530

    updates

--- order-service/src/main/java/com/clinic/orderservice/client/ProductClient.java
+++ order-service/src/main/java/com/clinic/orderservice/client/ProductClient.java
@@ -9,14 +9,13 @@ public class ProductClient {
 
     private final RestClient restClient;
 
-    public ProductClient(RestClient.Builder builder) {
-        this.restClient = builder
+    public ProductClient(RestClient.Builder builder){
+        this.restClient  = builder
                 .baseUrl("http://localhost:8081")
                 .build();
     }
 
-    public ProductResponse getProduct(Long productId) {
-
+    public ProductResponse getProduct(Long productId){
         return restClient
                 .get()
                 .uri("/products/{id}", productId)
